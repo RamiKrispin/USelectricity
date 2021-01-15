0c8b2873837d37117b5068a188431b0e54b4e04a
RamiKrispin <ramkrisp@umich.edu>
2021-01-15 08:00:22 -0800

    update the data

---
+++ docs/data_raw/data_refresh.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+Rscript -e "source('./functions/data_update.R');update_data()"
---
+++ docs/docker/dev/build_docker.sh
@@ -0,0 +1,10 @@
+echo "Build the docker"
+
+docker build . -t rkrispin/us_electricity:dev.0.0.0.9000
+
+if [[ $? = 0 ]] ; then
+echo "Pushing docker..."
+docker push rkrispin/us_electricity:dev.0.0.0.9000
+else
+echo "Docker build failed"
+fi
