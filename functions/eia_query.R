1daffd89a2dc8d1303d0d6efb52181758e661c14
RamiKrispin <ramkrisp@umich.edu>
2021-01-15 06:41:10 -0800

    add functions for pull and update the data

---
+++ functions/eia_query.R
@@ -0,0 +1,50 @@
+#' Pulling Data from EIA API
+#' @description The function enables to pull data from EIA API based on series ID
+#' @details The function send a GET request to the EIA API, parse the returned object from JSON to a clean data.frame object. More details:
+#' 
+#' - API documentation - https://www.eia.gov/opendata/commands.php
+#' - The API required a key, register here https://www.eia.gov/opendata/register.php
+#' - The function is using jq to parse the JSON - https://stedolan.github.io/jq/
+#' 
+#' Note: if pulling hourly data with UTC time zone setting, 
+#' a zulu time format (ISO 8601) should be use for the start and end argument.
+#' @param api_key EIA API key
+#' @param series_id Series ID
+#' @param start Optional, start time in a yyyymmddThhZ format (e.g., for 12 pm at January 1st, 2021 use 20210101T12Z). 
+#' See note about the time zone on the function details
+#' @param end Optional, end time in a yyyymmddThhZ format (e.g., for 12 pm at January 1st, 2021 use 20210101T12Z). 
+#' See note about the time zone on the function details
+#' @param tz The series index time zone, by default set to UTC
+#' @return a data.frame object
+
+
+eia_query <- function(api_key, 
+                      series_id, 
+                      start = NULL, 
+                      end = NULL, 
+                      tz = "UTC"){
+  `%>%` <- magrittr::`%>%`
+  
+  url <- base::paste("http://api.eia.gov/series/?",
+                     "api_key=", api_key,
+                     "&series_id=", series_id,
+                     "&out=json", sep = "")
+  
+  if(!is.null(start)){
+    url <- base::paste(url, "&start=", start, sep = "")
+  }
+  
+  if(!is.null(end)){
+    url <- base::paste(url, "&end=", end, sep = "")
+  }
+  
+  command <- base::paste("curl", " '",url, "' | jq -r '.series[].data[] | @tsv'", sep = "")
+  
+  output <- utils::read.table(text = system(command = command, intern = TRUE), sep = "\t") %>%
+    stats::setNames(c("timestamp", "series")) %>%
+    dplyr::mutate(date_time = lubridate::ymd_h(timestamp, tz = tz)) %>%
+    dplyr::select(date_time, series) %>%
+    dplyr::arrange(date_time)
+  
+  return(output)
+}
