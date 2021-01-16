f9683615d12f3f49040178d38b5b1b0465e24184
RamiKrispin <ramkrisp@umich.edu>
2021-01-15 20:25:25 -0800

    update the refresh function

--- data_raw/data_refresh.sh
+++ data_raw/data_refresh.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-Rscript -e "source('./functions/data_update.R');update_data()"
+Rscript -e "source('./functions/data_update.R');if(update_data()){rmarkdown::render_site()}"
+
+
+if [[ "$(git status --porcelain)" != "" ]]; then
+    git add docs/*
+    git add data/*
+    git commit -m "Auto update of the $1 data"
+    git push origin main
+else
+echo "Nothing to commit..."
+fi
