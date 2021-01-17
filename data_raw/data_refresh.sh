c6df632f77d8123f86d555e68c3d573c564ad050
Rami Krispin <12760966+RamiKrispin@users.noreply.github.com>
2021-01-16 22:53:26 -0800

    Update data_refresh.sh

--- data_raw/data_refresh.sh
+++ data_raw/data_refresh.sh
@@ -4,6 +4,8 @@ Rscript -e "source('./data_raw/data_refresh.R')" $2
 
 
 if [[ "$(git status --porcelain)" != "" ]]; then
+    git config --global user.name 'RamiKrispin'
+    git config --global user.email 'ramkrisp@umich.edu'
     git add docs/*
     git add data/*
     git commit -m "Auto update of the $1 data"
