1712b4984e281b618f324b4b5ab46b437510e141
RamiKrispin <ramkrisp@umich.edu>
2021-01-16 22:59:02 -0800

    add render_site to update the dashboard

--- data_raw/data_refresh.R
+++ data_raw/data_refresh.R
@@ -1,3 +1,8 @@
 api_key <- commandArgs(trailingOnly = TRUE)
 source('./functions/data_update.R')
-update_data(api_key = api_key[1])
+status <- FALSE
+status <- update_data(api_key = api_key[1])
+
+if(status){
+  rmarkdown::render_site()
+}
